62f3905118cb7e208aff8c5998eeeebbcf73fed1
Zachary Velcoff <33340277+zackfactual@users.noreply.github.com>
2018-03-02 17:08:07 -0800

    range() in Python
    playing with the range function

---
+++ rangeDrill.py
@@ -0,0 +1,14 @@
+# example
+myList = ['one','two','three','four','five']
+myListLen = len(myList)
+for i in range(0,myListLen):
+    print(myList[i])
+
+for i in range(0,4):
+    print(i)
+
+for i in range(3,-1,-1):
+    print(i)
+
+for i in range(8,0,-2):
+    print(i)
